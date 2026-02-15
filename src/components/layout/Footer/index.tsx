6182f8695cd591c6bb36bcf70160b302584f34ab
rafajperez <rafael_cordas@hotmail.com>
2026-02-15 10:38:14 -0300

    correçao

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
               <FaGithub size={18} />
             </a>
             <a
-              href="https://linkedin.com/in/seu-usuario"
+              href="https://www.linkedin.com/in/rafael-perez-998308a9"
               target="_blank"
               rel="noreferrer"
               className="hover:text-blue-500 transition-colors"
